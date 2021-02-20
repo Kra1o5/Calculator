f811fe194b9d8348c029dd8258ff80d60a31d2d9
Alejandro Lopez <kra1o5x@gmail.com>
2021-02-20 21:37:48 +0100

    Add Javadoc documentation

---
+++ target/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"lopez_carmona_alejandro.calculator"}];updateSearchResults();
---
+++ target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"lopez_carmona_alejandro.calculator","l":"Calculator"}];updateSearchResults();
